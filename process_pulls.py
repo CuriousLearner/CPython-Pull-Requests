b50affd57d4e1b1c74be76a66f101e01368e7204
Cheryl Sabella <cheryl.sabella@gmail.com>
2019-02-18 18:08:02 -0500

    Add text file of output

--- process_pulls.py
+++ process_pulls.py
@@ -6,9 +6,13 @@ from pprint import pprint
 with open('pull_requests.json', 'r') as input_file:
     pulls = json.load(input_file)
 
-file_to_pr = defaultdict(set)
-for pr, values in pulls.items():
-    for file in values['files']:
-        filename = file['filename']
-        file_to_pr[filename].add(pr)
+def pr_by_file():
+    files = defaultdict(set)
+    for pr, values in pulls.items():
+        for file in values['files']:
+            filename = file['filename']
+            files[filename].add(pr)
+    return files
+
+pprint(pr_by_file())
 
