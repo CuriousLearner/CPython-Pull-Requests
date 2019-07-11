8f4be4c3c021dcbeaa2ce2e7b8185b839bd12218
Mayank Singhal <17mayank.singhal@gmail.com>
2019-07-11 15:15:40 +0530

    Fetch and process PR data without files (#2)

--- process_pulls.py
+++ process_pulls.py
@@ -1,27 +1,22 @@
-import json
 from collections import defaultdict
 
-with open('pull_requests.json', 'r') as input_file:
-    pulls = json.load(input_file)
 
-
-def pr_by_file():
+def pr_by_file(pull_requests):
     files = defaultdict(set)
-    for pr in pulls:
+    for pr in pull_requests:
         for file in pr['files']['nodes']:
             filename = file['path']
             files[filename].add(pr['number'])
     return files
 
 
-def change_values_to_list():
-    files = pr_by_file()
+def change_values_to_list(pull_requests):
+    files = pr_by_file(pull_requests)
     for filename, pr in files.items():
         files[filename] = list(pr)
     files = dict(sorted(files.items()))
-    with open('pr_files.json', 'w') as outfile:
-        return json.dump(files, outfile)
+    return files
 
 
-if __name__ == "__main__":
-    change_values_to_list()
+def main(pull_requests):
+    return change_values_to_list(pull_requests)
--- pull_request_files.py
+++ pull_request_files.py
@@ -1,8 +1,13 @@
 import json
-from inspect import cleandoc
 import os
-import requests
+from inspect import cleandoc
 
+import requests
+from dotenv import find_dotenv, load_dotenv
+
+import process_pulls
+
+load_dotenv(find_dotenv())
 url = 'https://api.github.com/graphql'
 oauth_token = os.environ.get("GH_AUTH")
 headers = {'Authorization': f'bearer {oauth_token}'}
@@ -49,17 +54,18 @@ def get_pull_request_details(endCursor=None):
     return response
 
 
-page = 1
-print(f'[*] Fetching page {page}', end='\r')
-response = get_pull_request_details()
-pull_requests = response['data']['repository']['pullRequests']['nodes']
-print(f'[✓] Fetched page {page} ')
-while response['data']['repository']['pullRequests']['pageInfo']['hasNextPage']:
-    page = page + 1
-    print(f'[*] Fetching page {page}', end='\r')
-    response = get_pull_request_details(response['data']['repository']['pullRequests']['pageInfo']['endCursor'])
-    print(f'[✓] Fetched page {page} ')
-    pull_requests = pull_requests + response['data']['repository']['pullRequests']['nodes']
+def main():
+    page = 1
+    response = get_pull_request_details()
+    pull_requests = response['data']['repository']['pullRequests']['nodes']
+    while response['data']['repository']['pullRequests']['pageInfo']['hasNextPage']:
+        page = page + 1
+        response = get_pull_request_details(response['data']['repository']['pullRequests']['pageInfo']['endCursor'])
+        pull_requests = pull_requests + response['data']['repository']['pullRequests']['nodes']
 
-with open('pull_requests.json', 'w') as outfile:
-    json.dump(pull_requests, outfile)
+    files = process_pulls.main(pull_requests)
+    return files
+
+
+if __name__ == "__main__":
+    main()
